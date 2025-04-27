16c9079dc015cf2a5129b10887b5ab6be4844039
Julian <j.sp@tuta.io>
2025-04-27 17:14:01 +0000

    telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,15 +5,16 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-    telescope.setup {
-        defaults = {
-        },
-        pickers = {
-        },
-        extensions = {
-        },
+        telescope.setup {
+            defaults = {
+            },
+            pickers = {
+            },
+            extensions = {
+            },
+        }
 
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    }
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end,
 }
